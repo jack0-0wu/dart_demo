bcc2b845261a773a73dd0a7713061d4c5915d20c
JackWu <1121204968@qq.com>
2021-01-14 18:24:28 +0800

    1

--- bin/dart_demo.dart
+++ bin/dart_demo.dart
@@ -3,14 +3,46 @@ import 'dart:async';
 import 'package:dart_demo/dart_demo.dart' as dart_demo;
 
 void main(List<String> arguments) {
-  Future(() {
-    for (var i = 0; i < 1000000000; i++) {}
-    print('耗时任务结束');
-    
-    return '1';
+  // Future(() {
+  //   throw StateError('This is a Dart exception in Future.');
+  // }).then((value) => print(value), onError: (dynamic e, StackTrace stack) => print(e));
+  // Future(() {
+  //   throw StateError('This is a Dart exception in Future.');
+  // }).catchError((dynamic e, StackTrace stack) => print(e), test: (e) => true);
+  // Future(() {
+  // }).whenComplete(() => print(0));
+  // Future(() {
+  //   for (var i = 0; i < 10000000000; i++) {}
+  //   print('延时');
+  // }).timeout(Duration(microseconds: 1), onTimeout: () {
+  //   print('超时了');
+  //   return Future(() {
+  //     for (var i = 0; i < 1000000000; i++) {}
+  //     print('延时1');
+  //   });
+  // });
+
+  Future<bool>(() {
+    for (var i = 0; i < 10000000000; i++) {}
+    return true;
+  }).timeout(Duration(milliseconds: 2), onTimeout: () {
+    print('0 timed out');
+    return false;
   }).then((value) => print(value));
-  testFutureVoid();
-  print('main函数上的代码');
+
+}
+
+Future<bool> myTypedFuture(int id, int duration) async {
+  await Future.delayed(Duration(seconds: duration));
+  print('Delay complete for Future $id');
+  return true;
+}
+
+Future runTimeout() async {
+  await myTypedFuture(0, 10).timeout(Duration(seconds: 2), onTimeout: () {
+    print('0 timed out');
+    return false;
+  });
 }
 
 // Future<void> testFutureVoid() {
