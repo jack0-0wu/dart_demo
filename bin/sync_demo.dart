4530204e70792ccf79da63e329068da77892f5d1
JackWu <1121204968@qq.com>
2021-03-24 15:19:09 +0800

    生成器函数demo追加

--- bin/sync_demo.dart
+++ bin/sync_demo.dart
@@ -14,15 +14,29 @@
 //  print("DONE");
 //}
 
-void main() {
-  for (int i in numbersDownFrom(3)) {
-    print('$i');
-  }
+// void main() {
+//   for (int i in numbersDownFrom(3)) {
+//     print('$i');
+//   }
+// }
+//
+// Iterable<int> numbersDownFrom(int n) sync* {
+//   if (n != 0) {
+//     yield n;
+//     yield* numbersDownFrom(n - 1);
+//   }
+// }
+Stream<int> naturalsTo(int n) async* {
+  int k = 0;
+  while (k < n) yield k++;
 }
+void main() async{
+  Stream<int> sequence = naturalsTo(5);
 
-Iterable<int> numbersDownFrom(int n) sync* {
-  if (n != 0) {
-    yield n;
-    yield* numbersDownFrom(n - 1);
+  print("Starting...");
+
+  await for (int value in sequence) {
+    print(value);
   }
+  print("DONE");
 }
