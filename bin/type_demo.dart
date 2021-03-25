c3c93530367709bf5bd6a115552dc215d3289827
JackWu <1121204968@qq.com>
2021-03-25 18:05:20 +0800

    1

---
+++ bin/type_demo.dart
@@ -0,0 +1,17 @@
+void main(){
+  int a = 1;
+  double d = 2;
+
+  print(a.toDouble().runtimeType);
+  print(d.toInt().runtimeType);
+  int.parse("source");
+  double.parse("source");
+  num.parse("input");
+  num n = 5;
+  print(n.runtimeType);
+  n= 5.5;
+  print(n.runtimeType);
+  print(num.parse("1.5") is double);
+  double c = 2.0123456789012345678901234567890123456789012345678901234567890123;
+  print(c);
+}
