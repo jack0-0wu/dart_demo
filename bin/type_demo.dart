878a0cd234e2ec3ff0014372c4dc7a6a887d3130
JackWu <1121204968@qq.com>
2021-04-01 14:37:27 +0800

    1

--- bin/type_demo.dart
+++ bin/type_demo.dart
@@ -1,17 +1,20 @@
 void main(){
-  int a = 1;
-  double d = 2;
-
-  print(a.toDouble().runtimeType);
-  print(d.toInt().runtimeType);
-  int.parse("source");
-  double.parse("source");
-  num.parse("input");
-  num n = 5;
-  print(n.runtimeType);
-  n= 5.5;
-  print(n.runtimeType);
-  print(num.parse("1.5") is double);
-  double c = 2.0123456789012345678901234567890123456789012345678901234567890123;
-  print(c);
+  // int a = 1;
+  // double d = 2;
+  //
+  // print(a.toDouble().runtimeType);
+  // print(d.toInt().runtimeType);
+  // int.parse("source");
+  // double.parse("source");
+  // num.parse("input");
+  // num n = 5;
+  // print(n.runtimeType);
+  // n= 5.5;
+  // print(n.runtimeType);
+  // print(num.parse("1.5") is double);
+  // double c = 2.0123456789012345678901234567890123456789012345678901234567890123;
+  // print(c);
+  List test;
+  test = ["1","2","3"];
+  print(test);
 }
